85ed7799aedbb5f6bde9a74f22919ce2eff3cb98
Artem Emelin <artem.emelin@dataart.com>
2019-02-01 13:01:17 +0200

    Remote repo test

---
+++ README.md
@@ -0,0 +1,2 @@
+# corp-api-management
+eFax Corporate API combined management endpoints handling user admin and api application admin (replaces the new-ish repo "prodfax-admin")
---
+++ utils-aaa/src/main/java/com/test/utils/aaa/TestAAA.java
@@ -0,0 +1,11 @@
+package com.test.utils.aaa;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class TestAAA {
+
+    public static String aaa() {
+        return "This is REMOTE Repo";
+    }
+}
